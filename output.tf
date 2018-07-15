add80dd41722305c7d6ec7c29845d5f34c6423d3
will Farrell <will.farrell@gmail.com>
2018-07-15 14:41:32 -0600

    fix: output var

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "backend_s3_bucket" {
   value = "${aws_s3_bucket.main.id}"
 }
 output "backend_s3_bucket_logs" {
-  value = "${aws_s3_bucket.main_logs.id}"
+  value = "${module.logs.id}"
 }
 output "backend_s3_dynamodb_table" {
   value = "${aws_dynamodb_table.main.id}"
