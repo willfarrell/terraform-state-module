2da0bb65148de4b7eaeefb9ba63aafc506db0d62
will Farrell <willfarrell@users.noreply.github.com>
2019-06-16 08:29:36 -0600

    fix: add in public block

--- s3.tf
+++ s3.tf
@@ -37,6 +37,15 @@ resource "aws_s3_bucket" "main" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = "${aws_s3_bucket.main.id}"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+  
 // Causes: Failed to save state: failed to upload state: AccessDenied: Access Denied
 //	status code: 403, request id: ***********, host id: *********
 //resource "aws_s3_bucket_policy" "main" {
