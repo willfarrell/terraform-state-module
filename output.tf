6e8203f7f7bc2262ff63c781a25d4481c570d996
will Farrell <will.farrell@gmail.com>
2019-07-11 08:01:18 -0600

    fix: remove unused output

--- output.tf
+++ output.tf
@@ -2,10 +2,6 @@ output "s3_bucket_id" {
   value = aws_s3_bucket.main.id
 }
 
-output "s3_bucket_logs_id" {
-  value = module.logs.id
-}
-
 output "dynamodb_table_id" {
   value = aws_dynamodb_table.main.id
 }
