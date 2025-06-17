5a877dc7919ab43f75e2a52bc3dd458c83539bee
will Farrell <willfarrell@proton.me>
2025-06-17 06:45:11 -0600

    fix: remove deprecated
    Signed-off-by: will Farrell <willfarrell@proton.me>

--- output.tf
+++ output.tf
@@ -2,10 +2,6 @@ output "s3_bucket_id" {
   value = aws_s3_bucket.main.id
 }
 
-output "dynamodb_table_id" {
-  value = aws_dynamodb_table.main.id
-}
-
 /*
 output "kms_arn" {
   value = "${aws_kms_key.main.arn}"
